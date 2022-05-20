381c004c0863ef4da41300fbb3f86e820687b4b2
Mikhail Radchenko <misha-radchenko@yandex.ru>
2022-05-20 20:07:55 +0300

    feat: add another wallet address

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const ADDRESSES = [
   { address: '0x8a92E706cd359536D7A57dC9CC24054f7B17e021', type: 'contract' },
   { address: '0x0e2F752C845Bdb31368d7012CA93f45AF345Ec73', type: 'contract' },
   { address: '0xAc8892AC86bB02F26544F31af06b86fdD2105862', type: 'contract' },
+  { address: '0x2c6900b24221de2b4a45c8c89482fff96ffb7e55', type: 'contract' },
 ];
 
 const App = () => {
